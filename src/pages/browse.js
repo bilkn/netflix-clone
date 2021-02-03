aeb3531431aaafc0a6cdfaf10dca5bd3a8c134d5
bilkn <65674342+bilkn@users.noreply.github.com>
2021-02-03 18:33:48 +0300

    feat: add pages and routes

---
+++ src/pages/browse.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Browse() {
+  return <p>Browse</p>;
+}
+
+export default Browse;
---
+++ src/pages/signin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Signin() {
+  return <p>Signin</p>;
+}
+
+export default Signin;
